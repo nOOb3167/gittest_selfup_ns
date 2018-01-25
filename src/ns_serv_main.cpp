bc5b755d18ca3f1e3826033de4536f0df0020c71
nOOb3167 <nOOb3167@gmail.com>
2018-01-25 06:46:29 +0100

    selfup_ns: serv SELFUP_CMD_REQUEST_LATEST_COMMIT_TREE

--- src/ns_serv_main.cpp
+++ src/ns_serv_main.cpp
@@ -3,10 +3,13 @@
 #include <stdexcept>
 #include <thread>
 
+#include <selfup/ns_git_shims.h>
 #include <selfup/ns_helpers.h>
 #include <selfup/TCPAsync.h>
 
 #define SERVUP_ASYNC_TIMEOUT_MS 10000
+
+using namespace ns_git;
 
 typedef TCPAsync::Respond Respond;
 
@@ -46,9 +49,40 @@ private:
 	std::exception_ptr            m_thread_exc;
 };
 
+class ServupCacheHead
+{
+public:
+	ServupCacheHead(ns_git_oid commit_tree_oid, treemap_t trees) :
+		m_commit_tree_oid(commit_tree_oid),
+		m_trees(std::move(trees))
+	{}
+
+public:
+	ns_git_oid m_commit_tree_oid;
+	treemap_t  m_trees;
+};
+
 class ServupConExt2
 {
 public:
+	ServupConExt2() {};
+
+	void cacheHeadRefresh(ns_git_oid wanted_oid)
+	{
+		// FIXME: make cache head per-refname
+
+		if (m_cache_head && oid_comparator_t()(m_cache_head->m_commit_tree_oid, wanted_oid))
+			return;
+
+		treemap_t trees(treelist_recursive(m_repopath, wanted_oid));
+		std::shared_ptr<ServupCacheHead> cache_head(new ServupCacheHead(wanted_oid, std::move(trees)));
+
+		m_cache_head = cache_head;
+	}
+
+public:
+	std::string m_repopath;
+	std::shared_ptr<ServupCacheHead> m_cache_head;
 };
 
 class ServupWork2 : public ServupWork
@@ -61,7 +95,33 @@ public:
 
 	void virtualFrameDispatch(NetworkPacket *packet, Respond *respond) override
 	{
-		throw std::runtime_error("unimplemented");
+		uint8_t id;
+
+		(*packet) >> id;
+
+		switch (id)
+		{
+
+		case SELFUP_CMD_REQUEST_LATEST_COMMIT_TREE:
+		{
+			uint32_t refnum = 0;
+
+			(*packet) >> refnum;
+
+			std::string refname(packet->inSizedStr(refnum), refnum);
+			ns_git_oid latest_oid(latest_commit_tree_oid(m_ext->m_repopath, refname));
+
+			m_ext->cacheHeadRefresh(latest_oid);
+
+			NetworkPacket res_latest_pkt(SELFUP_CMD_RESPONSE_LATEST_COMMIT_TREE, networkpacket_cmd_tag_t());
+			res_latest_pkt.outSizedStr((char *) latest_oid.id, NS_GIT_OID_RAWSZ);
+			respond->respondOneshot(std::move(res_latest_pkt));
+		}
+		break;
+
+		default:
+			throw std::runtime_error("id");
+		}
 	}
 
 private:
