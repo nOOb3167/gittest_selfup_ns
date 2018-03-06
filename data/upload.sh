ec2cbdcd7a93e5b400062006a40e4786ac1d354d
nOOb3167 <nOOb3167@gmail.com>
2018-03-06 21:04:20 +0100

    selfup_ns: improve upload script

--- data/upload.sh
+++ data/upload.sh
@@ -6,7 +6,7 @@ CURRENTDIR=$(dirname $(readlink -e "$0"))
 [ -d "$CURRENTDIR" ] && cd "$CURRENTDIR" || exit 1;
 
 CFG_CMD_SSH="ssh -T -e none"
-CFG_CMD_SCP_RECURSIVE="scp -r"
+CFG_CMD_SCP="scp"
 CFG_CMD_BASH="bash -e -s -x"
 
 CFG_ARCHIVE_SELFUP=
@@ -21,15 +21,19 @@ CFG_REMOTE_REPO=
 [ -f "$CURRENTDIR/upload_config.inc" ] && . "$CURRENTDIR/upload_config.inc"
 
 [ -f "$CFG_ARCHIVE_SELFUP" ] && [ -f "$CFG_ARCHIVE_MAINUP" ] || exit 1
-[ -n "$CFG_REMOTE_ARCHIVE_SELFUP" ] && [ -n "$CFG_REMOTE_ARCHIVE_MAINUP" ] || exit 1
+[ -n "$CFG_REMOTE_ARCHIVE_DIR" ] || exit 1
 [ -n "$CFG_REMOTE_HOST" ] && [ -n "$CFG_REMOTE_REPO" ] || exit 1
 
-$CFG_CMD_SCP_RECURSIVE "$CFG_ARCHIVE_SELFUP" "$CFG_REMOTE_HOST:$CFG_REMOTE_ARCHIVE_SELFUP_WEB" >/dev/null 2>&1
-$CFG_CMD_SCP_RECURSIVE "$CFG_ARCHIVE_SELFUP" "$CFG_REMOTE_HOST:$CFG_REMOTE_ARCHIVE_SELFUP" >/dev/null 2>&1
-$CFG_CMD_SCP_RECURSIVE "$CFG_ARCHIVE_MAINUP" "$CFG_REMOTE_HOST:$CFG_REMOTE_ARCHIVE_MAINUP" >/dev/null 2>&1
+REMOTE_ARCHIVE_SELFUP="$CFG_REMOTE_ARCHIVE_DIR/$(basename "$CFG_ARCHIVE_SELFUP")"
+REMOTE_ARCHIVE_MAINUP="$CFG_REMOTE_ARCHIVE_DIR/$(basename "$CFG_ARCHIVE_MAINUP")"
+
+[ -n "$REMOTE_ARCHIVE_SELFUP" ] && [ -n "$REMOTE_ARCHIVE_MAINUP" ] || exit 1
+
+$CFG_CMD_SCP "$CFG_ARCHIVE_SELFUP" "$CFG_ARCHIVE_MAINUP" "$CFG_REMOTE_HOST:$CFG_REMOTE_ARCHIVE_DIR" >/dev/null 2>&1
 
 $CFG_CMD_SSH "$CFG_REMOTE_HOST" "$CFG_CMD_BASH" <<EOF
-	chmod 744 "$CFG_REMOTE_ARCHIVE_SELFUP_WEB"
+	chmod 744 "$REMOTE_ARCHIVE_SELFUP"
+	cp "$REMOTE_ARCHIVE_SELFUP" "$CFG_REMOTE_ARCHIVE_SELFUP_WEB"
 
 	cd "$CFG_REMOTE_REPO"
 	
@@ -41,7 +45,7 @@ $CFG_CMD_SSH "$CFG_REMOTE_HOST" "$CFG_CMD_BASH" <<EOF
 	git checkout --orphan tmp_selfup || true
 	git rm -rf . >/dev/null 2>&1 || true
 	
-	unzip "$CFG_REMOTE_ARCHIVE_SELFUP" >/dev/null 2>&1
+	unzip "$REMOTE_ARCHIVE_SELFUP" >/dev/null 2>&1
 	
 	git add --all
 	git commit -m"dummy" >/dev/null 2>&1
@@ -55,7 +59,7 @@ $CFG_CMD_SSH "$CFG_REMOTE_HOST" "$CFG_CMD_BASH" <<EOF
 	git checkout --orphan tmp_mainup || true
 	git rm -rf . >/dev/null 2>&1 || true
 	
-	unzip "$CFG_REMOTE_ARCHIVE_MAINUP" >/dev/null 2>&1
+	unzip "$REMOTE_ARCHIVE_MAINUP" >/dev/null 2>&1
 	
 	git add --all
 	git commit -m "dummy" >/dev/null 2>&1
